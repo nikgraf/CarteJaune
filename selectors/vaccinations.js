06a24a1574921c66a099b068f4d6901bc9dcedee
Nik Graf <nik@nikgraf.com>
2016-01-26 06:27:23 -0800

    fix(vaccinations): fix selector for case when no vaccinations exists

--- selectors/vaccinations.js
+++ selectors/vaccinations.js
@@ -1,11 +1,13 @@
 import { createSelector } from 'reselect';
 import shallowVaccines from './vaccines';
+import { Map } from 'immutable';
 
 const shallowVaccinations = (state) => state.get('vaccinations');
 
 export default createSelector(
   [shallowVaccinations, shallowVaccines],
   (vaccinations, vaccines) => {
+    if (!vaccinations) return Map();
     return vaccinations.map((vaccination, id) => {
       return vaccination
         .set('vaccine', vaccines.get(vaccination.get('id')))
